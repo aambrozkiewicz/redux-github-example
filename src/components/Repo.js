933b3686988c1c5589b1e0648f84913c7a28dd10
Aleksander Ambrozkiewicz <aambrozkiewicz@gmail.com>
2019-07-27 12:39:56 +0200

    add stargazers_count

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,11 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import { Icon } from "antd";
 
 const RepoItem = styled.div`
   text-align: left;
   margin: 0.5rem;
   padding: 0.5rem;
 `;
+
+const IconText = ({ type, text }) => (
+  <span>
+    <Icon type={type} style={{ marginRight: 8 }} />
+    {text}
+  </span>
+);
 
 export default function({ item }) {
   return (
@@ -14,6 +22,7 @@ export default function({ item }) {
         <a href={item.html_url}>{item.name}</a>
       </h3>
       <p>{item.description}</p>
+      <IconText type="star-o" text={item.stargazers_count} />
     </RepoItem>
   );
 }
